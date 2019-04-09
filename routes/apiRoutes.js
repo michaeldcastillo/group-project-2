9e2fc55a720214e5f112dec1615e24b0e21b033d
katiebug32 <katiebug32@gmail.com>
2019-04-08 22:00:02 -0500

    more api route adds and fixes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -2,6 +2,11 @@
 var db = require("../models");
 
 module.exports = function (app) {
+
+
+  ///////////// GETS ///////////////
+  
+
   // Get All Meals - front end can display all in dropdown, 
   // and only toMake=true in a display div
   app.get("/api/recipes", function (req, res) {
@@ -10,6 +15,35 @@ module.exports = function (app) {
     });
   });
 
+  // Get All Ingredients
+  app.get("/api/ingredients", function (req, res) {
+    db.Ingredient.findAll({}).then(function (dbIngredient) {
+      res.json(dbIngredient);
+    });
+  });
+
+
+  // Get all ingredients per a recipe
+  app.get("/api/recipes/:recipeID", function (req, res) {
+    // recipeID = req.params.recipeID;
+    db.Recipe.findAll({
+      where: {id: req.params.recipeID},
+      include: [{
+        model: db.Ingredient,
+        through: {
+          attributes: ["RecipeId", "IngredientId", "ingredient"]
+        }
+      }]
+    }).then(function (dbRecipe) {
+      //does this work, or do I need to pull more info from List table here??
+      res.json(dbRecipe);
+    });
+  });
+ 
+
+
+  ////////////// POSTS ////////////////
+
   // Create a new Recipe
   app.post("/api/recipes", function (req, res) {
     db.Recipe.create(req.body).then(function (dbNewMeal) {
@@ -17,25 +51,47 @@ module.exports = function (app) {
     });
   });
 
-  // // Create a new Ingredient - insert into Ingredient Table
-  // app.post("/api/ingredients", function (req, res) {
-  //   db.Ingredient.create(req.body).then(function (NewIngredient) {
-  //     res.json(NewIngredient);
-  //   });
-  // });
-
   // add ingredient to recipe
-  app.post("/api/recipes/:recipeID/ingredients/:ingredient", function (req, res) {
-    db.Ingredient.create(req.body).then(function (NewIngredient) {
-      // res.json(NewIngredient);
-      db.Recipe.findByPk(req.params.recipeID)
-        .then(function (recipe) {
-          recipe.setIngredients([NewIngredient.ingredientID]).then(function () {
-            res.json(NewIngredient);
+  app.post("/api/recipes/:recipeID", function (req, res) {
+    var ingredient = req.body.ingredient.toLowerCase();
+    // console.log(ingredient);
+    db.Ingredient.findOne({ where: {ingredient: ingredient } }).then(function(response) {
+      console.log("Hi");
+      if (response) {
+        var ingredientID = response.id;
+        console.log(ingredientID);
+        db.Recipe.findOne({ where: { id: req.params.recipeID } })
+          .then(function (recipe) {
+            recipe.addIngredients([ingredientID]).then(function () {
+              res.json("Linked ingredient to recipe.");
+            });
           });
+      } else {
+        // console.log(ingredient);
+        db.Ingredient.create({ingredient: ingredient}).then(function (newIngredient) {
+          // console.log(newIngredient);
+          db.Recipe.findOne({ where: { id: req.params.recipeID } })
+            .then(function (recipe) {
+              recipe.addIngredients([newIngredient.id]).then(function () {
+                res.json(newIngredient);
+              });
+            });
         });
+      }
     });
   });
+
+  // app.post("/api/recipes/:recipeID", function (req, res) {
+  //   db.Ingredient.create(req.body).then(function (newIngredient) {
+  //     // res.json(NewIngredient);
+  //     // req.params.ingredient = NewIngredient.ingredient;
+  //     console.log(newIngredient.id);
+  //     // db.List.create({ RecipeId: req.params.recipeID, IngredientID: newIngredient.id } )
+  //     //   .then(function () {
+  //     //     res.json(newIngredient);
+  //     //   });
+  //   });
+  // });
       
 
   // Create a new Ingredient - insert into Ingredient Table
