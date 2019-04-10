3ef8ed4aaddc221538fe6fb97f6f81280f9995dc
nolanglenn <nolangglenn@gmail.com>
2019-04-10 18:58:39 -0500

    added image folder

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -11,6 +11,17 @@ module.exports = function(app) {
     });
   });
 
+  // Load recipe page and pass in an recipe by id
+  app.get("/recipe/:id", function(req, res) {
+    db.Recipe.findOne({ where: { id: req.params.id } }).then(function(
+      dbRecipe
+    ) {
+      res.render("recipe", {
+        recipe: dbRecipe
+      });
+    });
+  });
+
   // Load example page and pass in an example by id
   app.get("/recipe/:id", function(req, res) {
     db.Recipe.findOne({ where: { id: req.params.id } }).then(function(
