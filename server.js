fdef8170e88333f65364521da55a68c54e4fc09f
Alton Carroll <chaos.five.zero@gmail.com>
2019-04-12 14:33:34 -0500

    Fixed layouts

--- server.js
+++ server.js
@@ -13,12 +13,11 @@ app.use(express.json());
 app.use(express.static("public"));
 
 // Handlebars
-app.engine(
-  "handlebars",
-  exphbs({
-    defaultLayout: "main"
-  })
-);
+app.engine("handlebars", exphbs({
+  defaultLayout: "main",
+  layoutsDir: __dirname + "/views/layouts/",
+  partialsDir: __dirname + "/views/partials/"
+}));
 app.set("view engine", "handlebars");
 
 // Routes
